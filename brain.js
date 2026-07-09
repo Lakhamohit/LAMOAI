fbaf8494e8cf99efad225ea853f31c875e36d992
Mohit Lakha <lakhwinderdasslakha7@gmail.com>
2026-07-09 14:14:50 +0530

    Update brain.js

--- brain.js
+++ brain.js
@@ -1,58 +1,85 @@
 async function think(question){
 
+    console.log("Brain started:", question);
+
 
     let answer;
 
 
+    try {
 
-    answer = conversation(question);
 
-    if(answer){
-        return answer;
+        console.log("Checking conversation");
+
+        answer = conversation(question);
+
+        if(answer){
+            return answer;
+        }
+
+
+
+        console.log("Checking datetime");
+
+        answer = datetime(question);
+
+        if(answer){
+            return answer;
+        }
+
+
+
+        console.log("Checking calculator");
+
+        answer = calculator(question);
+
+        if(answer){
+            return answer;
+        }
+
+
+
+        console.log("Checking facts");
+
+        answer = facts(question);
+
+        if(answer){
+            return answer;
+        }
+
+
+
+        console.log("Checking memory");
+
+        answer = memoryModule(question);
+
+        if(answer){
+            return answer;
+        }
+
+
+
+        console.log("Checking wiki");
+
+        answer = await wiki(question);
+
+        if(answer){
+            return answer;
+        }
+
+
+
+        return "No answer found.";
+
+
     }
 
+    catch(error){
 
+        console.log("BRAIN ERROR:", error);
 
-    answer = datetime(question);
+        return "Brain error detected.";
 
-    if(answer){
-        return answer;
     }
-
-
-
-    answer = calculator(question);
-
-    if(answer){
-        return answer;
-    }
-
-
-
-    answer = facts(question);
-
-    if(answer){
-        return answer;
-    }
-
-
-
-    answer = memoryModule(question);
-
-    if(answer){
-        return answer;
-    }
-
-
-
-    answer = await wiki(question);
-
-    if(answer){
-        return answer;
-    }
-
-
-
-    return "I could not find information about that.";
 
 }
